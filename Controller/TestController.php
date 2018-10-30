f880897d779b28d03451b258d46a99270a4948b8
afroj ahmad <afrojahmad070@gmail.com>
2018-10-30 21:33:09 +0530

    updates

---
+++ Controller/TestController.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Firoj\Eshop\Frontend\Controller;
+// use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class TestController extends Controller 
+{
+    public function index()
+    {
+        dump("fontend");die;
+    }
+}
+
+?>
---
+++ EshopFrontendBundle.php
@@ -0,0 +1,11 @@
+<?php 
+namespace Firoj\Eshop\Frontend;
+use symfony\Component\HttpKernal\Bundle\Bundle;
+
+
+class EshopFrontendBundle extends Bundle 
+{
+    
+}
+
+?>
