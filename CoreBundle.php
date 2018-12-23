46d79abe252a9f3b1785393ef0a424f423fd2824
afroj ahmad <afrojahmad070@gmail.com>
2018-12-23 23:42:56 +0530

    new updates

--- CoreBundle.php
+++ CoreBundle.php
@@ -2,10 +2,14 @@
 namespace Firoj\SchoolManagement\Core;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Firoj\SchoolManagement\Core\DependencyInjection\CoreExtension;
 
 class CoreBundle extends Bundle 
 {
-    
+    public function getContainerExtension()
+    {
+        return new CoreExtension();
+    }
 }
 
 ?>
